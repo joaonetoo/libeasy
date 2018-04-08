45da4ab15a919ec8a3ba517113d2f66cb6cf9605
Kalebe <lucaskalebe13@gmail.com>
2018-04-08 09:19:10 -0300

    mudanças em user

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -16,29 +16,45 @@ router.route('/users')
     	let login = req.body.login;
 		let email = req.body.email;
 		let cpf = req.body.cpf;
+		let first_name = req.body.first_name;
+		let last_name = req.body.last_name;
+		let endereço = req.body.endereço;
+		let birthday = req.body.birthday;
+		let type = req.body.type;
 		
 		
 		
     	// login check
-    	User.findOne({where:{login: login}, attributes: ['id', ['login', 'email', 'cpf']]}).then(user =>{
+    	User.findOne({where:{login: login}, attributes: ['id', ['login', 'email',]]}).then(user =>{
 			if(user){				
 				return res.send({message: 'Esse login já está em uso'});
 			}
 			else {
 				//email check
-				User.findOne({ where: { email: email }, attributes: ['id', ['login', 'email', 'cpf']] }).then(user => {
+				User.findOne({ where: { email: email }, attributes: ['id', ['login', 'email']] }).then(user => {
 					if (user) {
 						return res.json({ message: 'Esse email já está em uso' });
 					}
 					else {
 						//cpf check
-						User.findOne({ where: { cpf: cpf }, attributes: ['id', ['login', 'email', 'cpf']] }).then(user => {
+						User.findOne({ where: { cpf: cpf }, attributes: ['id', ['cpf','cpf']] }).then(user => {
 							if (user) {
 								return res.json({ message: 'Esse cpf já está em uso' });
 							}
 							else {
 								bcrypt.hash(req.body.password, 12).then((result) => {
-									User.create({ login: login, password: result, email: email }).then((u) => {
+									User.create({ 
+										login: login, 
+										password: result, 
+										email: email,
+										cpf:cpf,
+										first_name: first_name,
+										last_name: last_name,
+										endereço: endereço,
+										birthday: birthday,
+										type: type
+
+									 }).then((u) => {
 										res.json({ message: "User added", u });
 									})
 								});
@@ -56,29 +72,83 @@ router.route('/users')
 router.route('/users/:user_id')
 	.get((req, res) => {
 		User.findById(req.params.user_id).then(user =>{
-			res.json(user)
+			if(user){
+				res.json(user)
+			}
+			else{
+				res.json('Usuário não encontrado');
+			}
 			
 		})
 	})
+	// Modificar o usuário em caso de necessidade
 	.put((req, res) => {
-		User.findById(req.params.user_id).then(userEscolhido => {
-			if(userEscolhido) {
-				bcrypt.hash(req.body.password, 12).then((result) => {
-					userEscolhido.update({
-						login: req.body.login,
-						password: result, 
-						email: req.body.email
-					})
-				}).then(() => {
-					res.json(userEscolhido)
-				})} 
+		let login = req.body.login;
+		let email = req.body.email;
+		let cpf = req.body.cpf;
+		let first_name = req.body.first_name;
+		let last_name = req.body.last_name;
+		let endereço = req.body.endereço;
+		let birthday = req.body.birthday;
+		let type = req.body.type;
+		
+		
+		User.findById(req.params.user_id).then(userTest => { 
+		if(userTest){
 			
+			// login check
+			User.findOne({ where: { login: login }, attributes: ['id', ['login', 'email',]] }).then(user => {
+				if (user) {
+					res.send(userTest.id == user.id )
+					res.send({ message: 'Esse login já está em uso',user });
+				}
 				else {
-				res.json({ error: "Usuário não encontrado" })
+					//email check
+					User.findOne({ where: { email: email }, attributes: ['id', ['login', 'email']] }).then(user => {
+						if (user) {
+							return res.json({ message: 'Esse email já está em uso' });
+						}
+						else {
+							//cpf check
+							User.findOne({ where: { cpf: cpf }, attributes: ['cpf', ['cpf', 'cpf']] }).then(user => {
+								if (user) {
+									return res.json({ message: 'Esse cpf já está em uso' });
+								}
+								else {
+									bcrypt.hash(req.body.password, 12).then((result) => {
+										User.update({
+											login: login,
+											password: result,
+											email: email,
+											cpf: cpf,
+											first_name: first_name,
+											last_name: last_name,
+											endereço: endereço,
+											birthday: birthday,
+											type: type
+
+										}).then((u) => {
+											res.json({ message: "User added", u });
+										})
+									});
+
+								}
+							})
+
+						}
+					})
+
+				}
+			})
 			}
+		else{
+			res.json({ error: 'User não encontrado' })
+		
+		}
 		})
 	})
 	
+	// Apagar o usuário se existir
 	.delete((req,res) => {
 		User.findById(req.params.user_id).then(user =>{
 		if(user){
