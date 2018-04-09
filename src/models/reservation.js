28b2f243578e53d46d4af02bc5818126cbc0ea0e
gabrielmarquis <gabriel.araujo@dce.ufpb.br>
2018-04-09 15:00:48 -0500

    fix

--- src/models/reservation.js
+++ src/models/reservation.js
@@ -6,17 +6,7 @@ import {User} from './user'
 export let Reservation = sequelize.define('reservation',{
     date: {type: Sequelize.DATE, defaultValue: Sequelize.NOW} ,
     expired: {type: Sequelize.BOOLEAN, defaultValue: false}
-},{
-    getterMethods: {
-    validateReservation() {
-        let yourDate = this.date
-        let dateNow  = new Date()
-        if (dateNow.getDate() - yourDate.getDate() >= 2 ){
-            this.date =true
-            return true
-        }
-    }
-  }
+}
 })
 Reservation.belongsTo(Book)
 Reservation.belongsTo(User)
