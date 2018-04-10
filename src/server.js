8f1dac1abebdf3a8c8d5d3ab64621028058a213f
joaorafaelsoarest <joao.rafael@dce.ufpb.br>
2018-04-10 01:01:19 -0300

    fix

--- src/server.js
+++ src/server.js
@@ -3,10 +3,11 @@ import routesBook from './controllers/books';
 import routesCategory from './controllers/categorys';
 import routesAuthor from './controllers/authors';
 import routesUser from './controllers/users';
-import routesAuth from './controllers/auth'
-import routesMaterial from './controllers/materials'
-import routesEmprestimo from './controllers/emprestimo'
-import routesReservation from './controllers/reservations'
+import routesAuth from './controllers/auth';
+import routesMaterial from './controllers/materials';
+import routesRating from './controllers/ratings';
+import routesEmprestimo from './controllers/emprestimo';
+import routesReservation from './controllers/reservations';
 
 import bodyParser from 'body-parser';
 
@@ -22,10 +23,11 @@ app.use('/',routesUser);
 app.use('/',routesBook);
 app.use('/',routesCategory);
 app.use('/',routesAuthor);
-app.use('/',routesAuth)
-app.use('/',routesMaterial)
-app.use('/',routesEmprestimo)
-app.use('/',routesReservation)
+app.use('/',routesAuth);
+app.use('/',routesMaterial);
+app.use('/',routesRating);
+app.use('/',routesEmprestimo);
+app.use('/',routesReservation);
 
 
 
